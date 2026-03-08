840f18375ee9448cb7c3486552d941c9a932d69f
Pierre Crochelet <crochelet.pierre@icloud.com>
2026-03-08 09:52:55 +0100

    Initial blueprint reset

--- chess/main.py
+++
@@ -1,9 +0,0 @@
-from chess.game import Game
-import logging
-
-logger = logging.Logger(__name__)
-
-if __name__=="__main__":
-    logger.info("Let's play a game of chess")
-    game = Game()
-    game.play()
